dc77dcbb3ea34e76a29966c76058d881bf57f3d4
MrSheruo <atomicx4pro@gmail.com>
2024-02-16 04:06:30 +0200

    changing the ext

--- src/data/random.ts
+++ src/data/random.ts
@@ -2,8 +2,7 @@ export const ads = [
   {
     id: 1,
     title: "Arcane",
-    image: "/ads/arcane.webp",
-    hoverColor: "#ba7a62",
+    image: "/ads/arcane.jpg",
     youtubeLink: "https://www.youtube.com/watch?v=fXmAurh012s",
   },
 ];
