ddc63031c233817f6dcb9f033af79c23573be3ed
MrSheruo <atomicx4pro@gmail.com>
2024-02-16 04:10:12 +0200

    changing dirctions

--- src/data/random.ts
+++ src/data/random.ts
@@ -2,7 +2,7 @@ export const ads = [
   {
     id: 1,
     title: "Arcane",
-    image: "/ads/arcane.webp",
+    image: "/trailers/arcane.webp",
     youtubeLink: "https://www.youtube.com/watch?v=fXmAurh012s",
   },
 ];
