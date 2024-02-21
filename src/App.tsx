9061b6c2f21080525cba7dd3ec63754bc8740c12
MrSheruo <atomicx4pro@gmail.com>
2024-02-21 05:59:39 +0200

    Updating credits to fix scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,6 @@ import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 export default function App() {
   return (
     <>
-      <div className="bg-blue-400 flex items-center justify-center h-12 font-semibold text-white rounded-b-md">
-        Made with ❤
-        <a
-          href="https://ahmed-yasser-portfolio.vercel.app/"
-          className="underline hover:no-underline ml-[1ch] hover:text-secondary-hover"
-        >
-          Ahmed Yasser
-        </a>
-        {/* Icons for social media */}
-        <div className="flex gap-4 ml-4">
-          <a
-            href="https://www.facebook.com/profile.php?id=100088503545195"
-            target="_blank"
-          >
-            <FaFacebook className="w-6 h-6  hover:text-secondary-hover" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/ahmed-yasser-77135621b/"
-            target="_blank"
-          >
-            <FaLinkedin className="w-6 h-6  hover:text-secondary-hover" />
-          </a>
-          <a href="https://github.com/MrSheruo" target="_blank">
-            <FaGithub className="w-6 h-6  hover:text-secondary-hover" />
-          </a>
-        </div>
-      </div>
       <SidebarProvider>
         <div className="max-h-screen flex flex-col">
           <PageHeader />
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -3,55 +3,85 @@ import { ArrowLeft, Menu, Search, User } from "lucide-react";
 import Button from "../components/Button";
 import { useState } from "react";
 import { useSidebarContext } from "../contexts/sidebarContext";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function PageHeader() {
   const [showSearchSection, setShowSearchSection] = useState(false);
   return (
-    <div className="z-50 sticky top-0 bg-white flex gap-10 lg:gap-20 justify-between py-2 mb-6 mx-4 shadow-sm">
-      <PageHeaderFirstSection hidden={showSearchSection} />
-      <form
-        className={`  gap-4 flex-grow justify-center ${
-          showSearchSection ? "flex" : "hidden md:flex"
-        }`}
-      >
-        {showSearchSection && (
+    <div className="z-50 sticky top-0">
+      <div className="bg-blue-400 flex items-center sticky top-0 justify-center h-12 font-semibold text-white rounded-b-md">
+        Made with ❤
+        <a
+          href="https://ahmed-yasser-portfolio.vercel.app/"
+          className="underline hover:no-underline ml-[1ch] hover:text-secondary-hover"
+        >
+          Ahmed Yasser
+        </a>
+        {/* Icons for social media */}
+        <div className="flex gap-4 ml-4">
+          <a
+            href="https://www.facebook.com/profile.php?id=100088503545195"
+            target="_blank"
+          >
+            <FaFacebook className="w-6 h-6  hover:text-secondary-hover" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ahmed-yasser-77135621b/"
+            target="_blank"
+          >
+            <FaLinkedin className="w-6 h-6  hover:text-secondary-hover" />
+          </a>
+          <a href="https://github.com/MrSheruo" target="_blank">
+            <FaGithub className="w-6 h-6  hover:text-secondary-hover" />
+          </a>
+        </div>
+      </div>
+      <div className=" bg-white flex gap-10 lg:gap-20 justify-between py-2 mb-6 mx-4 shadow-sm">
+        <PageHeaderFirstSection hidden={showSearchSection} />
+        <form
+          className={`  gap-4 flex-grow justify-center ${
+            showSearchSection ? "flex" : "hidden md:flex"
+          }`}
+        >
+          {showSearchSection && (
+            <Button
+              type="button"
+              size={"icon"}
+              variant={"ghost"}
+              className="flex-shrink-0"
+              onClick={() => setShowSearchSection(false)}
+            >
+              <ArrowLeft />
+            </Button>
+          )}
+          <div className="flex flex-grow max-w-[600px]">
+            <input
+              type="text"
+              placeholder="Search for movies"
+              className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+            />
+            <Button className="pt-2 px-4 rounded-r-full border border-secondary-border border-l-0 flex-shrink-0">
+              <Search />
+            </Button>
+          </div>
+        </form>
+        <div
+          className={` flex-shrink-0 md:gap-2 ${
+            showSearchSection ? "hidden" : "flex"
+          }`}
+        >
           <Button
-            type="button"
             size={"icon"}
             variant={"ghost"}
-            className="flex-shrink-0"
-            onClick={() => setShowSearchSection(false)}
+            className="md:hidden"
+            onClick={() => setShowSearchSection(true)}
           >
-            <ArrowLeft />
-          </Button>
-        )}
-        <div className="flex flex-grow max-w-[600px]">
-          <input
-            type="text"
-            placeholder="Search for movies"
-            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
-          />
-          <Button className="pt-2 px-4 rounded-r-full border border-secondary-border border-l-0 flex-shrink-0">
             <Search />
           </Button>
+          <Button size={"icon"} variant={"ghost"}>
+            <User />
+          </Button>
         </div>
-      </form>
-      <div
-        className={` flex-shrink-0 md:gap-2 ${
-          showSearchSection ? "hidden" : "flex"
-        }`}
-      >
-        <Button
-          size={"icon"}
-          variant={"ghost"}
-          className="md:hidden"
-          onClick={() => setShowSearchSection(true)}
-        >
-          <Search />
-        </Button>
-        <Button size={"icon"} variant={"ghost"}>
-          <User />
-        </Button>
       </div>
     </div>
   );
